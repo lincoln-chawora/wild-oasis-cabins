e87058ef1e09064fe672f9cbd496857f307c87c9
Lincoln Chawora <lincoln_1996@hotmail.com>
2024-08-28 11:02:00 +0100

    Add settings form to settings page and show settings data on form and add the ability to update it

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,21 +1,45 @@
 import Form from '../../ui/Form.jsx';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import {useCustomQuery} from "../../hooks/useCustomQuery.js";
+import {getSettings} from "../../services/apiSettings.js";
+import Spinner from "../../ui/Spinner.jsx";
+import {useCustomQueryClient} from "../../hooks/useCustomQueryClient.js";
+import {updateSetting as updateSettingApi} from "../../services/apiSettings.js";
 
 function UpdateSettingsForm() {
+    const {
+        isLoading,
+        data: {
+            breakfastPrice, maxBookingLength, minBookingLength, maxGuestsPerBooking
+        } = {},
+    } = useCustomQuery('settings', getSettings);
+
+    const {mutate: updateSetting, isLoading: isUpdating} = useCustomQueryClient('settings', updateSettingApi, 'Setting successfully updated.')
+
+    if (isLoading) return  <Spinner />
+
+    function handleUpdate(e, field) {
+        const value = e.target.value;
+
+        if (!value) return;
+
+        updateSetting({[field]: value});
+    }
+
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+        <Input type='number' id='min-nights' disabled={isUpdating} onBlur={(e) => handleUpdate(e, 'minBookingLength')} defaultValue={minBookingLength} />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+        <Input type='number' id='max-nights' disabled={isUpdating} onBlur={(e) => handleUpdate(e, 'maxBookingLength')} defaultValue={maxBookingLength} />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+        <Input type='number' id='max-guests' disabled={isUpdating} onBlur={(e) => handleUpdate(e, 'maxGuestsPerBooking')} defaultValue={maxGuestsPerBooking} />
       </FormRow>
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+        <Input type='number' id='breakfast-price' disabled={isUpdating} onBlur={(e) => handleUpdate(e, 'breakfastPrice')} defaultValue={breakfastPrice} />
       </FormRow>
     </Form>
   );
--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -84,7 +84,7 @@ function MainNav() {
                 <li>
                     <StyledNavLink to='/settings'>
                         <HiOutlineCog6Tooth />
-                        <span>Bookings</span>
+                        <span>Settings</span>
                     </StyledNavLink>
                 </li>
             </NavList>
